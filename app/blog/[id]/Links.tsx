52d232de71fba3f8c6ca13f7cb38d74e3b4338f6
Arthur Vincent Simon <developarvin@gmail.com>
2024-03-28 15:33:27 +0800

    Implement links

--- app/blog/[id]/Links.tsx
+++ app/blog/[id]/Links.tsx
@@ -2,15 +2,20 @@
 
 import Link from "next/link";
 import React from "react";
-import {usePathname} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 
 export function BlogLinks() {
     const pathname = usePathname()
+    const router = useRouter()
 
     return <div>
         <ul>
             <li><Link href="/blog/1" className={pathname == '/blog/1' ? 'bg-red-100' : ''}>Go to First Blog</Link></li>
-            <li><Link href="/blog/2" className={pathname == '/blog/2' ? 'bg-red-100' : ''}>Go to Second Blog</Link></li>
+            <li>
+                <button type="button" onClick={() => router.push('/blog/2')} className={pathname == '/blog/2' ? 'bg-red-100' : ''}>
+                    Go to Second Blog
+                </button>
+            </li>
         </ul>
     </div>
 }
