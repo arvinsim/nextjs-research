6a40ea6140e24550d452a05074656799652c0cac
Arthur Vincent Simon <developarvin@gmail.com>
2024-04-01 17:23:38 +0800

    Create route group, marketing

---
+++ app/(marketing)/landing-page/page.tsx
@@ -0,0 +1,6 @@
+export default function LandingPage() {
+    return <div>
+        <h1>Welcome to our website</h1>
+        <p>Learn more about our company and our products.</p>
+    </div>
+}
---
+++ app/(marketing)/press-releases/page.tsx
@@ -0,0 +1,8 @@
+export default function PressReleasesPage() {
+    return (
+        <div>
+            <h1>Press Releases</h1>
+            <p>Read the latest news from our company.</p>
+        </div>
+    )
+}
